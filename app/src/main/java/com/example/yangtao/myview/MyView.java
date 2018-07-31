b30cd530bd1432421f425634e2bd366ec3897ac1
yangtao <yangtao870505@126.com>
2018-07-31 17:37:32 +0800

    first commit

---
+++ app/src/main/java/com/example/yangtao/myview/MyView.java
@@ -0,0 +1,31 @@
+package com.example.yangtao.myview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MyView extends View {
+    private Paint mPaint;
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(getResources().getColor(android.R.color.holo_orange_light));
+        canvas.drawCircle(200, 200, 100, mPaint);
+    }
+
+    private void init() {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setAlpha(88);
+        mPaint.setColor(getResources().getColor(android.R.color.holo_green_light));
+    }
+}
