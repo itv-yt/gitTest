3a8afa8c2aaddf620dff90050b707a5d8e2fbe3b
yangtao <yangtao870505@126.com>
2018-08-03 15:59:22 +0800

    第二次提交，画线

--- app/src/main/java/com/example/yangtao/myview/MyView.java
+++ app/src/main/java/com/example/yangtao/myview/MyView.java
@@ -21,11 +21,13 @@ public class MyView extends View {
         super.onDraw(canvas);
         canvas.drawColor(getResources().getColor(android.R.color.holo_orange_light));
         canvas.drawCircle(200, 200, 100, mPaint);
+        canvas.drawLine(20, 300, 700, 605, mPaint);
     }
 
     private void init() {
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setAlpha(88);
+        mPaint.setStrokeWidth(5);
         mPaint.setColor(getResources().getColor(android.R.color.holo_green_light));
     }
 }
